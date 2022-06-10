d3393f8d6133c7409c05317368bd67676ecc5c06
sadanand pandey <pandeysadanad75@gmail.com>
2022-06-11 00:02:40 +0530

    book read operation completed

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+
+
+def book_view(request):
+
+    book_list = Book.objects.all()
+    context = {"data": book_list}
+    return render(request, "books/books.html", context)
+    # if request.POST:
+    #     pass
+    # if request.PUT:
+    #     pass
+    # if request.DELETE:
+    #     pass
--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books'
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'library_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
