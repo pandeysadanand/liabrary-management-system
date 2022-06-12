9d59e6d7c9f6faf07f80144ec4dc78a6da84fcf0
sadanand <pandeysadanand75@gmail.com>
2022-06-12 10:33:46 +0530

    jinja templates url implemented

--- books/models.py
+++ books/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name} by {self.author}"
